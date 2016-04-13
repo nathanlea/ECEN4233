357c717bf8c8678e9b56706f68825897c9307d29
Nathan <nathan.lea@okstate.edu>
2016-04-13 09:21:53 -0500

    Removed Git Failed merge lines Removed ECEN4233 Directory

--- mux51/mux51.sv
+++ mux51/mux51.sv
@@ -9,10 +9,6 @@ module mux51(A, B, C, D, E, Q, S);
    mux21 first(A, B, W, S[0]);
    mux21 second(C, D, X, S[0]);
    mux21 third(W, X, Y, S[1]);
-<<<<<<< HEAD
-   mux21 fourth(Y, E, Q, S[2]);
-=======
    mux21 fourth(E, Y, Q, S[2]);
->>>>>>> e337c3dcc28ecb0dfd271251269d4eee925e96a3
 
 endmodule // mux51
